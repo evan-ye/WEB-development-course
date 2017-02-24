6ed18ef4d8afb7af7819402c7ce5cfe38ba77ceb
Andrey Babenko <andreyb1990@gmail.com>
2017-02-24 15:29:52 +0200

    Add one line version of function getCentury()

--- Javascript Course/Homework/Lesson_1/Andrey_Babenko.js
+++ Javascript Course/Homework/Lesson_1/Andrey_Babenko.js
@@ -5,3 +5,5 @@ function getCentury(year) {
     return "Год указан не правильно";
   }
 }
+// One line version
+let getCenturyOneLine = (year) => +year >= 1  && +year <=2017 ? Math.ceil(year/100) : "Год указан не правильно";
