f2fd339cd42d7018f56e05917238cecb64072bca
ZhannaShulga <gusiksunny@gmail.com>
2017-02-27 23:09:04 +0200

    Fix bug

--- Javascript Course/Homework/Lesson_1/Zhanna_Shulga.js
+++ Javascript Course/Homework/Lesson_1/Zhanna_Shulga.js
@@ -2,7 +2,7 @@ function whatcentury(year){
  var mdate = new Date();
  var syear = year.toString();
  syear = '0'+'0'+'0'+syear;
- if(year<1 && year>mdate.getFullYear()) return 'Год указан неправильно!';
+ if(year<1 || year>mdate.getFullYear()) return 'Год указан неправильно!';
  else 
   if(syear.substr(syear.length-2,2)>0) return parseInt(syear.substr(syear.length-4,2))+1;
  else return parseInt(syear.substr(syear.length-4,2));
