dd26ee26f0aa096871679485e3285b6ea810ca1a
Zhanna <gusiksunny@gmail.com>
2017-03-07 10:07:36 +0200

    Create Zhanna_Shulga_RE.js

---
+++ Javascript Course/Homework/Lesson_3/Zhanna_Shulga_RE.js
@@ -0,0 +1,23 @@
+function Re(code)
+{ 
+  if (code == '' || code == undefined) {
+    return'Введите строку!';
+  }
+
+  var finalcode = '<ul>\n';
+  var result = code.match(/([\w\d\s\-\,]+)(?=<\/h2>)/gim);
+
+  if (result == null) {
+    return'В строке нет тегов <h2>!';
+  }
+
+  for (var i = 0; i < result.length;i++)  {
+     finalcode + '<li>' + result[i] + '</li>\n';
+  }
+
+  finalcode += '</ul>';
+  return finalcode;
+}
+var code = prompt('Введите код:', '');
+alert(Re(code));
+
