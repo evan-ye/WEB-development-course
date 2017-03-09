9ad6ccd5c77fb409442a39dcec3b2999414d9658
Misha Kuzmich <muuuzmich@gmail.com>
2017-03-09 12:25:49 +0200

    Fixed Reg.js

--- Javascript Course/Homework/Lesson_3/Misha_Kuzmich_Reg.js
+++ Javascript Course/Homework/Lesson_3/Misha_Kuzmich_Reg.js
@@ -1,11 +1,16 @@
-function getH2Content(html) {
-	var reg = /<[hH]2>[a-zA-Z ]*<\/[hH]2>/gm;
-	var arr = html.match(reg);
-	var line = '<ul>';
-	for(i = 0; i < arr.length; i++){
-		line += '<li>' + arr[i] + '</li>';
+function h2contentToList(text){
+	if(!text){
+		return 'No text recived.'
 	}
-	line += '</ul>';
-	return line;
+	reg = />([\w\d\s\-]+)(<\/h2>)/igm;
+	arr = htmlContent.match(reg);
+
+	list = '<ul>';
+	arr.forEach(function(item) {
+		list += item.replace('>', '<li>').replace('</h2>', '</li>');
+	}); 
+	list += '</ul>';
+
+	return list;
 }
-document.body.innerHTML = getH2Content('Name : <h2>John</h2> Second-name : <h2>Doe</h2>Specialisation: <h2> Developer </h2>')
+h2contentToList(htmlContent);
