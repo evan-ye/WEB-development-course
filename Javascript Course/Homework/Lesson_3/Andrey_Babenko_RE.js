57269dc49b466ba40d4966b3371617808b33096f
Andrey Babenko <andreyb1990@gmail.com>
2017-03-09 10:00:51 +0200

    Andrey_Babenko_RE.js update

--- Javascript Course/Homework/Lesson_3/Andrey_Babenko_RE.js
+++ Javascript Course/Homework/Lesson_3/Andrey_Babenko_RE.js
@@ -1,11 +1,15 @@
 function getElementsList(code) {
-  var regExp = /(?!>)([^><]*)(?=<\/h2>)/igm,
-      allElements = regExp.exec(code),
+  if (typeof(code) != 'string') {
+    return 'It is not string';
+  }
+  var regExp = /(?!>)([\w\d\s\-]+)(?=<\/h2>)/igm,
+      allElements = code.match(regExp),
       elementsList = '';
-  
+  if (allElements === null) {
+    return 'Tag h2 not found';
+  }
   for (var i = 0; i<allElements.length; i++) {
     elementsList += '<li>' + allElements[i] + '</li>\n';
   }
-  
   return '<ul>\n' + elementsList + '</ul>';
 }
