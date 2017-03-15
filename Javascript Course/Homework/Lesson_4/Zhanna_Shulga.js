123559d5ba4eac44ed8f4028b1b8884d10bd488d
Zhanna <gusiksunny@gmail.com>
2017-03-15 12:35:16 +0200

    Create Zhanna_Shulga.js

---
+++ Javascript Course/Homework/Lesson_4/Zhanna_Shulga.js
@@ -0,0 +1,11 @@
+function compare(a, b) { return a-b; }
+
+function missedNumbers(ar){
+  var finish = Math.max.apply(null, ar);
+  var start = Math.min.apply(null, ar);
+  var kol = finish - start + 1;
+  return kol - ar.length;
+ }
+
+var ar = [6, 2, 3, 8];
+alert('Не хватает ' + missedNumbers(ar) + ' элементов.');
