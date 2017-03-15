3ff8c4d1d7fb1b51cab25451908cd0c5c12a65e9
Zhanna <gusiksunny@gmail.com>
2017-03-15 20:04:43 +0200

    Update Zhanna_Shulga.js

--- Javascript Course/Homework/Lesson_4/Zhanna_Shulga.js
+++ Javascript Course/Homework/Lesson_4/Zhanna_Shulga.js
@@ -1,8 +1,16 @@
 function missedNumbers(ar){
   var finish = Math.max.apply(null, ar);
   var start = Math.min.apply(null, ar);
+  ar2 = ar.slice();
+  ar2.sort();
+  i = ar2.length;
+  while (i--) {
+    if (ar2[i] == ar2[i - 1]){
+        ar2.splice(i, 1);
+    }
+  }
   var kol = finish - start + 1;
-  return kol - ar.length;
+  return kol - ar2.length;
  }
 
 var ar = [6, 2, 3, 8];
