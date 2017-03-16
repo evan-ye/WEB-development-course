fabb9052971bc78e6b0ac417ef470bacf9d9e954
Klim Trakht <klim@kultprosvet.net>
2017-03-16 23:54:20 +0200

    Add jQuery lesson 5 code

--- jQuery Course/Lessons/assets/process.php
+++ jQuery Course/Lessons/assets/process.php
@@ -1,3 +1,21 @@
 <?php
 
-echo "Submission is successful";
+// Text Response
+$textResponse = "Submission is successful";
+
+// XML Responce
+$xmlResponse = '
+<?xml version="1.0" ?>
+<message id="234">
+ <from>Bob</from>
+ <to>Janette</to>
+ <subject>Hi Janette</subject>
+ <content>Janette, let\'s grab lunch today.</content>
+</message>';
+
+// JSON responce
+$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+$jsonResponse = json_encode($arr);
+
+
+echo  $textResponse;
