3162812dc3ca0d0cfbfd3f36fe734f9d7c56b302
Andrey Babenko <andreyb1990@gmail.com>
2017-03-19 23:28:38 +0200

    Code optimization

--- Javascript Course/Homework/Lesson_5/Andrey_Babenko.js
+++ Javascript Course/Homework/Lesson_5/Andrey_Babenko.js
@@ -7,16 +7,12 @@ function possibleSequence(array) {
     if (array[0] >= array[1]) {
       mistake++;
       array.splice(0,1);
-    } else if (array[i] <= array[i+1] && array[i] <= array[i-1]) {
+    } else if (array[i] <= array[i+1] && array[i] <= array[i-1] || array[i] >= array[i+1] && array[i] >= array[i+2]) {
       mistake++;
       array.splice(i,1);
       i--;
     } else if (array[i] < array[i+1]) {
       continue;
-    } else if (array[i] >= array[i+1] && array[i] >= array[i+2]) {
-      mistake++;
-      array.splice(i,1);
-      i--;
     } else {
       mistake++;
       array.splice(i+1,1);
