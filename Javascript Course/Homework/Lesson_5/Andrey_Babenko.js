a2889022f801f81cde1d044c539fff2317254087
Andrey Babenko <andreyb1990@gmail.com>
2017-03-19 23:19:43 +0200

    Bug fix

--- Javascript Course/Homework/Lesson_5/Andrey_Babenko.js
+++ Javascript Course/Homework/Lesson_5/Andrey_Babenko.js
@@ -4,12 +4,16 @@ function possibleSequence(array) {
   }
   var mistake = 0;
   for (var i=0; i<array.length-1; i++) {
-    if (array[0] > array[1]) {
+    if (array[0] >= array[1]) {
       mistake++;
       array.splice(0,1);
+    } else if (array[i] <= array[i+1] && array[i] <= array[i-1]) {
+      mistake++;
+      array.splice(i,1);
+      i--;
     } else if (array[i] < array[i+1]) {
       continue;
-    } else if (array[i] > array[i+1] && array[i] > array[i+2]) {
+    } else if (array[i] >= array[i+1] && array[i] >= array[i+2]) {
       mistake++;
       array.splice(i,1);
       i--;
