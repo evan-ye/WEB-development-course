e1f72e8bb4e1e1120cc71b5f6494b1b7a99609cb
evan-ye <text2evan@gmail.com>
2017-03-19 20:28:47 +0200

    Update Eugene_Yegorochkin.js

--- Javascript Course/Homework/Lesson_5/Eugene_Yegorochkin.js
+++ Javascript Course/Homework/Lesson_5/Eugene_Yegorochkin.js
@@ -4,12 +4,12 @@ function numb(arr) {
         return false
     }
     var count = [];
-    
+
     for (var i = 0; i < arr.length - 1; i++) {
 
         if (arr.length == 2 && arr[0] >= arr[1]) {
             return false
-        } else if (arr[i] >= arr[i + 1] || arr[i] === undefined || arr[arr.length - 1] === undefined) {
+        } else if (arr[i] >= arr[i + 1]) {
             count.push(1)
         } else if (arr[i] >= arr[i + 1] || arr[i] >= arr[i + 2]) {
             arr.splice(i + 2, 1)
