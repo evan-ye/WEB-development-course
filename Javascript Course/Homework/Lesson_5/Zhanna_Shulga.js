92123f5ed8f579105bf0aef9a8d87997ce454089
Zhanna <gusiksunny@gmail.com>
2017-03-19 21:07:50 +0200

    Zhanna_Shulga.js

---
+++ Javascript Course/Homework/Lesson_5/Zhanna_Shulga.js
@@ -0,0 +1,30 @@
+function isSequence(ar){ 
+if (ar.length <= 2) { 
+  return 'Введите массив, содержащий более двух элементов.';
+} 
+var i = 0; 
+while (i < ar.length - 1) { 
+  if (!i) {
+    if (ar[0] >= ar[1]) { 
+    ar.splice(0, 1); 
+    break; 
+    } 
+  } else if (ar[i] >= ar[i + 1] && ar[i] >= ar[i + 2]) { 
+    ar.splice(i, 1); 
+    break; 
+  } 
+    else if (ar[i] > ar[i+1]) {
+    ar.splice(i + 1, 1); 
+    break; 
+  }
+  i++;
+} 
+for (i = 0; i < ar.length-1; i++){ 
+  if (ar[i] >= ar[i+1]) {
+    return false;
+  } 
+} 
+return true; 
+} 
+var ar = [ 1, 2, 10, 5, 6] ;
+isSequence(ar);
