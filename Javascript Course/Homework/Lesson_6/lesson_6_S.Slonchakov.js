f696c05be7082e4a81403261a79053d298ce63ab
slonyaka <slonchakov@gmail.com>
2017-03-24 17:26:19 +0200

    Fixes for lesson's 6 code

--- Javascript Course/Homework/Lesson_6/lesson_6_S.Slonchakov.js
+++ Javascript Course/Homework/Lesson_6/lesson_6_S.Slonchakov.js
@@ -1,33 +1,59 @@
 function priceSumm(input){
 
-	if( !Array.isArray(input) || !input.length) return 'Wrong input';
+	var options= {
+		floorMax: 5,
+		aprtmentMax: 5,
+		priceMax: 10,
 
-	var summ = 0, floorLength = 0;
+	};
 
-	if(input.length == 1) {
+	var messages = {
 
-		return summ = input[0].reduce(function(a,b){return a+b;});
+		LongMatrix: 'Quantity of floors has to be less then 6',
+		LongFloor: 'Quantity of apartments has to be less then 6',
+		highPrice: 'Price of apartment has to be less then 11',
+		lowFloorsLength: 'Quantity of floors has to be more then 1'
 
 	}
 
-	floorLength = input[0].length;
+	var error = '';
+
+	if( !Array.isArray(input) || !input.length) return messages.lowFloorsLength;
+
+	if(input.length > options.floorMax ) return messages.LongMatrix;
+	if(input[0].length > options.aprtmentMax ) return messages.LongFloor;
+
+	var summ = input[0].reduce(function(a,b){return a+b;}), floorLength = 0;
+
+	if(input.length == 1) return summ;
+
+	floorLength = (input[0].length > options.aprtmentMax) ? options.aprtmentMax : input[0].length;
 
     input.reduce(function(previousValue, currentValue, index, array) {
-
+    	
     	for(var i = 0; i < floorLength; i++){
 
-			(previousValue[i] == 0) ? array[index][i] = 0 : summ += previousValue[i];
+			if(currentValue[i] > options.priceMax) error = messages.highPrice;
+
+			( previousValue[i] != 0 )? summ += currentValue[i] : currentValue[i] = 0;
 
     	}
 
 	  	return currentValue;
 	});
 
+	if(errors.length){
+		return error;
+	}
+
     return summ;
 }
+
+console.log(priceSumm([[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]])); // 9
+console.log(priceSumm([[0, 1, 1, 2], [0, 5, 1, 1], [2,4,1,1],[2, 1, 3, 3]])); // 24
 
 console.log(priceSumm("Fake input")); 
 console.log(priceSumm([])); 
 console.log(priceSumm([[0, 1, 1, 2]])); 
-console.log(priceSumm([[0, 1, 1, 2], [0, 5, 0, 0], [2, 0, 3, 3]])); // 9
-console.log(priceSumm([[0, 1, 1, 2], [0, 5, 0, 0], [2,4,1,1],[2, 0, 3, 3]])); // 13
+console.log(priceSumm([[0, 1, 1, 2],[0, 1, 1, 2],[0, 1, 1, 2],[0, 1, 1, 2],[0, 1, 1, 2],[0, 1, 1, 2]])); // count of floors is 6
+console.log(priceSumm([[0, 1, 1, 2],[0, 1, 1, 11],[0, 1, 1, 2],[0, 1, 1, 2],[0, 1, 1, 2]]));  // price more then 10
