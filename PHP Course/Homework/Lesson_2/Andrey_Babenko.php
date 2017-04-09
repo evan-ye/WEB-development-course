ce45ed07dd34b5b947ad837e9a71f9089528e032
Andrey Babenko <andreyb1990@gmail.com>
2017-04-09 19:19:41 +0300

    Code optimization

--- PHP Course/Homework/Lesson_2/Andrey_Babenko.php
+++ PHP Course/Homework/Lesson_2/Andrey_Babenko.php
@@ -2,13 +2,8 @@ function getEditedString($string) {
   if (!strpos($string, "(")) {
    return $string;
   }
-  if (strrpos($string, "(") < strpos($string, ")")) {
-    $start = strrpos($string, "(");
-    $end = strpos($string, ")");
-  } else {
-    $start = strpos($string, "(");
-    $end = strpos($string, ")");
-  }
+  $start = strrpos($string, "(") < strpos($string, ")") ? strrpos($string, "(") : strpos($string, "(");
+  $end = strpos($string, ")");
   $subStr = substr($string, $start+1, $end-$start-1);
   $string = str_replace("(".$subStr.")", strrev($subStr), $string);
   return getEditedString($string);
