b77cf111d6ff473a7c9d8f4c7432ffb2e3cfbb76
Andrey Babenko <andreyb1990@gmail.com>
2017-04-09 19:13:55 +0300

    No RegExp version

--- PHP Course/Homework/Lesson_2/Andrey_Babenko.php
+++ PHP Course/Homework/Lesson_2/Andrey_Babenko.php
@@ -1,18 +1,15 @@
-<?php
 function getEditedString($string) {
-  preg_match_all('/\((.+)\)/', $string, $matches);
-  for ($i=0; $i<count($matches[1]); $i++) {
-    if (strpos($matches[1][$i], "(") > strpos($matches[1][$i], ")")) {
-      preg_match_all('/\((.+)\)/U', $matches[0][$i], $matches2);
-      for ($j=0; $j<count($matches2[1]); $j++) {
-        $matches2[1][$j] = getEditedString($matches2[1][$j]);
-        $string = str_replace($matches2[0][$j], strrev($matches2[1][$j]), $string);
-      }
-    } else {
-      $matches[1][$i] = getEditedString($matches[1][$i]);
-      $string = str_replace($matches[0][$i], strrev($matches[1][$i]), $string);
-    }
+  if (!strpos($string, "(")) {
+   return $string;
   }
-  return $string;
+  if (strrpos($string, "(") < strpos($string, ")")) {
+    $start = strrpos($string, "(");
+    $end = strpos($string, ")");
+  } else {
+    $start = strpos($string, "(");
+    $end = strpos($string, ")");
+  }
+  $subStr = substr($string, $start+1, $end-$start-1);
+  $string = str_replace("(".$subStr.")", strrev($subStr), $string);
+  return getEditedString($string);
 }
-?>
