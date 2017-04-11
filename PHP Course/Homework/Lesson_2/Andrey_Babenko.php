ea80eabe3139a27f313ef4ff8c017c545382afaa
Andrey Babenko <andreyb1990@gmail.com>
2017-04-11 14:32:55 +0300

    Add new version

--- PHP Course/Homework/Lesson_2/Andrey_Babenko.php
+++ PHP Course/Homework/Lesson_2/Andrey_Babenko.php
@@ -1,10 +1,15 @@
 function getEditedString($string) {
-  if (!strpos($string, "(")) {
-   return $string;
+ $open = [];
+ $closed = [];
+  for ($i=0; $i<strlen($string); $i++) {
+    if ($string[$i] == "(") $open[] = $i;
+    if ($string[$i] == ")") {
+      $lastOpen = array_pop($open);
+      $search = substr($string, $lastOpen, $i-$lastOpen+1);
+      $replace = strrev(substr($string, $lastOpen+1, $i-$lastOpen-1));
+      $string = str_replace($search, $replace, $string);
+      $i -=2;
+    }
   }
-  $start = strrpos($string, "(") < strpos($string, ")") ? strrpos($string, "(") : strpos($string, "(");
-  $end = strpos($string, ")");
-  $subStr = substr($string, $start+1, $end-$start-1);
-  $string = str_replace("(".$subStr.")", strrev($subStr), $string);
-  return getEditedString($string);
+  return $string;
 }
